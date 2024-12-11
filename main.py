012931bade3c06e37d56266cd12966d995988b74
SarcasticSir <andreasantbro@gmail.com>
2024-12-11 22:00:30 +0100

    Legger til input tekst

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     path = "books/Frankenstein.txt"
     text = open_book(path)
     word_count = count_words(text)
+    print(f"Counting the words in {path}")
+    input("Press Enter to show results")
     print()
     print(f"The number of words are {word_count}")
 main()
