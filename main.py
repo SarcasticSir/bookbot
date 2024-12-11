c17b35f32c9ef7312ec32babb243cf16391691e0
SarcasticSir <andreasantbro@gmail.com>
2024-12-11 22:28:27 +0100

    Legger til visning av tittel på tekst

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
+
+
 # Function to open text file
 def open_book(path): # path to text file
     with open(path) as f: 
-        return f.read() 
+        return f.read()
+    
+# Removes file extension, returning just the title    
+def get_title(path):
+    filename = os.path.basename(path)
+    title, _ = os.path.splitext(filename)
+    return title
     
     
 def count_words(text):
@@ -12,7 +21,9 @@ def main():
     path = "books/Frankenstein.txt"
     text = open_book(path)
     word_count = count_words(text)
-    print(f"Counting the words in {path}")
+    title = get_title(path)
+
+    print(f"Counting the words in {title}")
     input("Press Enter to show results")
     print()
     print(f"The number of words are {word_count}")
